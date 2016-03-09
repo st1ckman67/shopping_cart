d1ed3a05469a6453fe1aa28fae989dbdbb28c4c3
Steve Hawkes <stevehawkes67@gmail.com>
2016-03-09 20:00:28 +0000

    Updated to use Classes

---
+++ sales/shopping_order.py
@@ -0,0 +1,21 @@
+class Order:
+
+    def __init__(self):
+        self.quit = False
+        self.add = False
+        self.delete = False
+        self.item = None
+
+    def get_input(self):
+        print("[command] [item] (command is 'a'dd, 'd'elete, 'p'rint cart, 'q'uit) ")
+        line = input()
+
+        command = line[:1]
+        self.item = line[2:]
+
+        if command == "a":
+            self.add = True
+        elif command == "d":
+            self.delete = True
+        elif command == "q":
+            self.quit = True
