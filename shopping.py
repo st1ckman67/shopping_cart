2c7253017969886e576f479642b278ef5dc9d158
Steve Hawkes <stevehawkes67@gmail.com>
2016-03-09 19:57:43 +0000

    Updated to use Classes

--- shopping.py
+++ shopping.py
@@ -1,53 +1,13 @@
-class Cart:
+import sales.shopping_cart
+import sales.shopping_order
 
-    def __init__(self):
-        self._contents = dict()
-
-    def __repr__(self):
-        return "{0} {1}".format(Cart, self.__dict__)
-
-    def process(self, order):
-        if order.add:
-            if order.item not in self._contents:
-                self._contents[order.item] = 0
-            self._contents[order.item] += 1
-        elif order.delete:
-            if order.item in self._contents:
-                self._contents[order.item] -= 1
-                if self._contents[order.item] <= 0:
-                    del self._contents[order.item]
-
-
-class Order:
-
-    def __init__(self):
-        self.quit = False
-        self.add = False
-        self.delete = False
-        self.item = None
-
-    def get_input(self):
-        print("[command] [item] (command is 'a'dd, 'd'elete, 'p'rint cart, 'q'uit) ")
-        line = input()
-
-        command = line[:1]
-        self.item = line[2:]
-
-        if command == "a":
-            self.add = True
-        elif command == "d":
-            self.delete = True
-        elif command == "q":
-            self.quit = True
-
-
-cart = Cart()
-order = Order()
+cart = sales.shopping_cart.Cart()
+order = sales.shopping_order.Order()
 order.get_input()
 
 while not order.quit:
     cart.process(order)
-    order = Order()
+    order = sales.shopping_order.Order()
     order.get_input()
 
 print(cart)
